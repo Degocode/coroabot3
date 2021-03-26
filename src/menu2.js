61d16589e2c9bb4c06750f3c4f6ee4384ae13cda
Degocode <80612531+Degocode@users.noreply.github.com>
2021-03-26 10:27:05 -0300

    Update menu2.js

--- src/menu2.js
+++ src/menu2.js
@@ -45,6 +45,6 @@ const menu2 = (prefix) => {
 ╠➸ *${prefix}simih 0 (para desativar)*
 ╠➥ *${prefix}simi (sua mensagem)*
 ║
-╠══✪〘 *CAUSS BOT* 〙✪══`
+╠══✪〘 *Andre Noah* 〙✪══`
 }
 exports.menu2 = menu2
